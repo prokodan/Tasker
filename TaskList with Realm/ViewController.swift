78c6e6bc956b052662009aacd707a2b9ca8c7c7c
Prokopenko Danil <prokodan@gmail.com>
2022-10-05 20:18:28 +0600

    Initial Commit

---
+++ TaskList with Realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskList with Realm
+//
+//  Created by Данил Прокопенко on 05.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
